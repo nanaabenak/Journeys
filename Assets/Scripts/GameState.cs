3c74546582eca36068cf53f01ae394abca7282e8
Nana Abena Kissi-Boateng <nanaabenabk@gmail.com>
2026-03-11 06:10:18 +0000

    Task 2 - Game State singleton created

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public static GameState instance; // Singleton instance of the GameState class
+    public string currentCountry;
+    public int storyProgress;
+    public int decisionMade;
+
+
+    void Awake()
+    {
+        // Debug.Log("GameState created: " + currentCountry);
+
+        if (instance == null) // If there is no instance of game state, set this as the instance
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Don't destroy this game object when loading a scene   
+        }
+        else
+        {
+            Destroy(gameObject); // If there is already an instance of game state, destroy this game object
+        }
+    }
+
+   
+}
